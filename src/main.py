a6691ff648953c1f9f2830efdacdf4ecd9d0c1b9
manh-td <dmtran120901@gmail.com>
2025-11-10 10:32:39 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -56,7 +56,7 @@ def main():
 
     rows = []
     if response.status_code == 200:
-        words = response.text.splitlines()[:2]
+        words = response.text.splitlines()
         for word in words:
             word = word.split(" ")[0].strip()
             if not word:
